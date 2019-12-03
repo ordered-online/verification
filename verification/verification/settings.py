a51f45565d395820ba4cb7f6d6aad3028468ccef
Felix Kästner <kaestner.fe@gmail.com>
2019-12-03 18:11:10 +0100

    Setup support for proxy headers

--- verification/verification/settings.py
+++ verification/verification/settings.py
@@ -126,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Setup support for proxy headers
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
